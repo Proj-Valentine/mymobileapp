fb8a058502782b31b8826e8bd6bfa7725f8558aa
VKAmpah <vkampah28@gmail.com>
2023-11-05 17:49:25 -0330

    setting up  sign up forms using shadcn and zod validator

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -11,10 +11,15 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ):(
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             {/* render what has to be on the page */}
+            {/* applying styling to center form element */}
             <Outlet />
           </section>
+          <img
+            src = "/assets/images/side-img.svg"
+            alt="log"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"/>
         </>
       )}
     </>
