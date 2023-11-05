d3b6391af0dc791d04614fba732a8dade6124a08
VKAmpah <vkampah28@gmail.com>
2023-11-05 14:02:48 -0330

    Baes structure complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './globals.css';
 
 import { Routes, Route } from 'react-router-dom';
+// without curly braces implies default import
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SigninForm';
+// with curly braces implies name import, helps to import more pages on one line
+import { Home } from './_root/pages';
+
 
 const App = () => {
   return (
    <main className="flex h-screen">
     <Routes>
         {/* public routes: these are routes everyone can seeeg sign in page/home page of app */}
-        <Route path="/sign-in" element={<SignForm} />
+        <Route path="/sign-in" element={<SigninForm/>} />
+        <Route path="/sign-up" element={<SignupForm/>} />
 
         {/* private routes: see only when you sign in ie personal profile*/}
-        <Route index element={<Home />}/>
     </Routes>
+        <Route index element={<Home/>}/>
 
    </main>
   )
