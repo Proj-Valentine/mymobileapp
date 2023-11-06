b106fac3c62a001a7922cb61f89f8c1eb4096bd5
VKAmpah <vkampah28@gmail.com>
2023-11-06 00:35:28 -0330

    defining zod form validation object

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+
+// define a form validation schema to reuse
+export const SignupValidation = z.object({
+    name: z.string().min(2,{message: 'Too short: name must be more than 2 characters'} ),
+    // username: z.string().min(2).max(50),
+    username: z.string().min(2, {message:'Too short: username must be more than 2 characters'}),
+    email: z.string().email(),
+    password: z.string().min(8,{message: 'password must not be less than 8 characters'})
+
+})
