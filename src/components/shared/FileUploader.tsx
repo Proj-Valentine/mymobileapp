90de045ec13c16b65f2ffb9081bdce9d0f75f8f4
VKAmpah <vkampah28@gmail.com>
2023-11-10 16:08:50 -0330

    Add styling to default upload field

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,9 +1,12 @@
 //  Note all this is from the documentationS page
 
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
 
 const FileUploader = () => {
+
+    const [fileUrl, setFileUrl] = useState('')
 
      const onDrop = useCallback((acceptedFiles) => {
        // Do something with the files
@@ -14,13 +17,27 @@ const FileUploader = () => {
      });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
+    <div
+      {...getRootProps()}
+      className=" flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? <div>test1</div> : (
+      <div className="file_uploader-box">
+        <img src="/assets/icons/file-upload.svg"
+        width={96}
+        height={77}
+        alt='file upload'
+        />
+        <h3 className="base-medium text-light-2 mb-2 mt-6"> Drag photo here</h3>
+        <p className="text-light-4 small-regular mb-6"> SVG,PNG,JPG </p>
+        <Button className="shad-button_dark_4"> Select from computer</Button>
+      </div>)}
+      {/* {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      )} */}
     </div>
   );
 };
