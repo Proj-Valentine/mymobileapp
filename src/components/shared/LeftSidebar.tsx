cc7806463e61591e0200ea767dd59acd3216d0ce
VKAmpah <vkampah28@gmail.com>
2023-11-10 13:05:04 -0330

    finish Leftside bar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -39,43 +39,53 @@ const LeftSidebar = () => {
               className="h-14 w-14 rounded-full"
             />
             <div className="flex flex-col">
-                <p className="body-bold">
-                    {user.name}
-                </p>
-                <p className="small-regular text-light-3">
-                    @{user.username}
-                </p>
+              <p className="body-bold">{user.name}</p>
+              <p className="small-regular text-light-3">@{user.username}</p>
             </div>
           </Link>
 
           <ul className="flex flex-col gap-6">
-            {sidebarLinks.map((link: INavLink)=> {
-                // is Active will return a boolean to check if pathname ie contains the loaction ie url of an element
-                const isActive = pathname === link.route
-                return (
-                  <li
-                    key={link.label}
-                    // adding a background color to show the link/page we are on (that is active) 
-                    // if we are on the page useLocation variable will return the url of the page ie the route
-                    // adding group class to extend the hover from li to the children
-                    className={`leftsidebar-link group ${isActive && 'bg-primary-500' }`}
+            {sidebarLinks.map((link: INavLink) => {
+              // is Active will return a boolean to check if pathname ie contains the loaction ie url of an element
+              const isActive = pathname === link.route;
+              return (
+                <li
+                  key={link.label}
+                  // adding a background color to show the link/page we are on (that is active)
+                  // if we are on the page useLocation variable will return the url of the page ie the route
+                  // adding group class to extend the hover from li to the children
+                  className={`leftsidebar-link group ${
+                    isActive && "bg-primary-500"
+                  }`}
+                >
+                  <NavLink
+                    to={link.route}
+                    className="flex gap-4 items-center p-4"
                   >
-                    <NavLink
-                      to={link.route}
-                      className="flex gap-4 items-center p-4"
-                    >
-                      <img
-                        src={link.imgURL}
-                        alt={link.label}
-                        className={`group-hover:invert-white ${isActive && 'invert-white'}`}
-                      />
-                      {link.label}
-                    </NavLink>
-                  </li>
-                );
+                    <img
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`group-hover:invert-white ${
+                        isActive && "invert-white"
+                      }`}
+                    />
+                    {link.label}
+                  </NavLink>
+                </li>
+              );
             })}
-
           </ul>
+        </div>
+
+        <div>
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+            <p className="small-medium lg:base-medium"> Logout</p>
+          </Button>
         </div>
       </nav>
     </div>
