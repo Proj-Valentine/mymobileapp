6095098e4dccf09a0cdd20f6ad527df0f2528f5f
VKAmpah <vkampah28@gmail.com>
2023-11-17 18:25:21 -0330

    Add toast to undeveloped pages

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,10 +1,28 @@
 
-const AllUsers = () => {
+import { Button } from "@/components/ui/button";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+
+export function AllUsers() {
+  const { toast } = useToast();
+  
+
   return (
-    <div>
-      Allusers
-    </div>
-  )
+    <Button
+      variant="outline"
+      onClick={() => {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "PAGE UNDER DEVELOPMENT.",
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+          duration: 30000
+        });
+      }}
+    >
+      Click Me
+    </Button>
+  );
 }
 
-export default AllUsers
+export default AllUsers;
--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,10 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+import { ToastAction } from "@radix-ui/react-toast";
 
 const Profile = () => {
   return (
     <div>
-      profile
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: "PAGE UNDER DEVELOPMENT.",
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+            duration: 30000,
+          });
+        }}
+      >
+        Click Me
+      </Button>
     </div>
-  )
+  );
 }
 
 export default Profile
